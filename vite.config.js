575bbe5670516891198887de200535c0efd75a68
lidong <lidong@smalltreemedia.com>
2025-04-22 10:39:36 +0800

    no message

--- vite.config.js
+++ vite.config.js
@@ -11,24 +11,13 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html')
+        main: resolve(__dirname, 'index.html'),
+        help: resolve(__dirname, 'googleff03b3026faa6312.html')
       }
     }
   },
   server: {
     port: 3000,
     open: true
-  },
-  // 自定义构建钩子，把静态文件复制过去
-  plugins: [
-    {
-      name: 'copy-static-files',
-      buildEnd() {
-        const filesToCopy = ['sitemap.xml', 'robots.txt',"googleff03b3026faa6312.html"]
-        for (const file of filesToCopy) {
-          fs.copyFileSync(resolve(__dirname, file), resolve(__dirname, 'dist', file))
-        }
-      }
-    }
-  ]
+  }
 })
